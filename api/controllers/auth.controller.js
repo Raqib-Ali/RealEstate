2f60ea238d65b6677a2634caa07069a2d06a0bf3
Raqib-Ali <aliraqib138@gmail.com>
2025-01-08 13:42:46 +0530

    hashed the password using bcrypt handled error usingtry catch

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,21 @@
 import User from "../models/user.model.js";
+import bcrypt from "../../node_modules/bcrypt/bcrypt.js"
+import { errorHandler } from "../util.js/error.js";
 
-export const signup =  async (req, res) => {
+export const signup = async (req, res, next) => {
 
-    const {username, email, password} = req.body;
-    const userName = new User({username, email, password})
-    await userName.save();
-    res.status(201).json('User created successfully');
+    const { username, email, password } = req.body;
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const userName = new User({ username, email, password: hash })
+
+
+    try {
+        await userName.save();
+        res.status(201).json('User created successfully');
+    } catch (error) {
+       next(errorHandler(550, "Error from the function"));
+    }
 
 };
